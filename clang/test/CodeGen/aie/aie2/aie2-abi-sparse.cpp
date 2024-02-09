5195825035236c8bcb748cbc875f293ab8d03c5e
Hamza Khallouki <hamza.khallouki@amd.com>
2024-02-09 13:42:24 +0000

    [AIE2] Define sparse types as structures of 512-bit vector and 128-bit mask types

--- clang/test/CodeGen/aie/aie2/aie2-abi-sparse.cpp
+++ clang/test/CodeGen/aie/aie2/aie2-abi-sparse.cpp
@@ -12,6 +12,14 @@
 
 extern "C" {
 
+// CHECK: %struct.v128int8_sparse = type <{ <64 x i8>, i128 }>
+// CHECK: %struct.v128uint8_sparse = type <{ <64 x i8>, i128 }>
+// CHECK: %struct.v256int4_sparse = type <{ <64 x i8>, i128 }>
+// CHECK: %struct.v256uint4_sparse = type <{ <64 x i8>, i128 }>
+// CHECK: %struct.v64int16_sparse = type <{ <32 x i16>, i128 }>
+// CHECK: %struct.v64uint16_sparse = type <{ <32 x i16>, i128 }>
+// CHECK: %struct.v64bfloat16_sparse = type <{ <32 x bfloat>, i128 }>
+
 // CHECK-LABEL: define {{[^@]*}}i128 @ret_sparsity_t
 // CHECK-SAME: () #[[ATTR0:[0-9]+]] {
 sparsity_t ret_sparsity_t(void) { return {}; }
@@ -19,5 +27,63 @@ sparsity_t ret_sparsity_t(void) { return {}; }
 // CHECK-LABEL: define {{[^@]*}}void @pass_sparsity_t
 // CHECK-SAME: (i128 noundef [[TMP0:%.*]]) #[[ATTR0:[0-9]+]] {
 void pass_sparsity_t(sparsity_t) {}
+
+// CHECK-LABEL: define {{[^@]*}}void @ret_v128int8_sparse
+// CHECK-SAME: (ptr noalias sret([[STRUCT_V128INT8_SPARSE:%.*]]) align 16 [[AGG_RESULT:%.*]]) #[[ATTR0:[0-9]+]] {
+v128int8_sparse ret_v128int8_sparse(void) { return {}; }
+
+// CHECK-LABEL: define {{[^@]*}}void @pass_v128int8_sparse
+// CHECK-SAME: ([[STRUCT_V128INT8_SPARSE:%.*]] [[DOTCOERCE:%.*]]) #[[ATTR0:[0-9]+]] {
+void pass_v128int8_sparse(v128int8_sparse) {}
+
+// CHECK-LABEL: define {{[^@]*}}void @ret_v128uint8_sparse
+// CHECK-SAME: (ptr noalias sret([[STRUCT_V128UINT8_SPARSE:%.*]]) align 16 [[AGG_RESULT:%.*]]) #[[ATTR0:[0-9]+]] {
+v128uint8_sparse ret_v128uint8_sparse(void) { return {}; }
+
+// CHECK-LABEL: define {{[^@]*}}void @pass_v128uint8_sparse
+// CHECK-SAME: ([[STRUCT_V128UINT8_SPARSE:%.*]] [[DOTCOERCE:%.*]]) #[[ATTR0:[0-9]+]] {
+void pass_v128uint8_sparse(v128uint8_sparse) {}
+
+
+// CHECK-LABEL: define {{[^@]*}}void @ret_v256int4_sparse
+// CHECK-SAME: (ptr noalias sret([[STRUCT_V256INT4_SPARSE:%.*]]) align 16 [[AGG_RESULT:%.*]]) #[[ATTR0:[0-9]+]] {
+v256int4_sparse ret_v256int4_sparse(void) { return {}; }
+
+// CHECK-LABEL: define {{[^@]*}}void @pass_v256int4_sparse
+// CHECK-SAME: ([[STRUCT_V256INT4_SPARSE:%.*]] [[DOTCOERCE:%.*]]) #[[ATTR0:[0-9]+]] {
+void pass_v256int4_sparse(v256int4_sparse) {}
+
+// CHECK-LABEL: define {{[^@]*}}void @ret_v256uint4_sparse
+// CHECK-SAME: (ptr noalias sret([[STRUCT_V256UINT4_SPARSE:%.*]]) align 16 [[AGG_RESULT:%.*]]) #[[ATTR0:[0-9]+]] {
+v256uint4_sparse ret_v256uint4_sparse(void) { return {}; }
+
+// CHECK-LABEL: define {{[^@]*}}void @pass_v256uint4_sparse
+// CHECK-SAME: ([[STRUCT_V256UINT4_SPARSE:%.*]] [[DOTCOERCE:%.*]]) #[[ATTR0:[0-9]+]] {
+void pass_v256uint4_sparse(v256uint4_sparse) {}
+
+
+// CHECK-LABEL: define {{[^@]*}}void @ret_v64int16_sparse
+// CHECK-SAME: (ptr noalias sret([[STRUCT_V64INT16_SPARSE:%.*]]) align 16 [[AGG_RESULT:%.*]]) #[[ATTR0:[0-9]+]] {
+v64int16_sparse ret_v64int16_sparse(void) { return {}; }
+
+// CHECK-LABEL: define {{[^@]*}}void @pass_v64int16_sparse
+// CHECK-SAME: ([[STRUCT_V64INT16_SPARSE:%.*]] [[DOTCOERCE:%.*]]) #[[ATTR0:[0-9]+]] {
+void pass_v64int16_sparse(v64int16_sparse) {}
+
+// CHECK-LABEL: define {{[^@]*}}void @ret_v64uint16_sparse
+// CHECK-SAME: (ptr noalias sret([[STRUCT_V64UINT16_SPARSE:%.*]]) align 16 [[AGG_RESULT:%.*]]) #[[ATTR0:[0-9]+]] {
+v64uint16_sparse ret_v64uint16_sparse(void) { return {}; }
+
+// CHECK-LABEL: define {{[^@]*}}void @pass_v64uint16_sparse
+// CHECK-SAME: ([[STRUCT_V64UINT16_SPARSE:%.*]] [[DOTCOERCE:%.*]]) #[[ATTR0:[0-9]+]] {
+void pass_v64uint16_sparse(v64uint16_sparse) {}
+
+// CHECK-LABEL: define {{[^@]*}}void @ret_v64bfloat16_sparse
+// CHECK-SAME: (ptr noalias sret([[STRUCT_V64BFLOAT16_SPARSE:%.*]]) align 16 [[AGG_RESULT:%.*]]) #[[ATTR0:[0-9]+]] {
+v64bfloat16_sparse ret_v64bfloat16_sparse(void) { return {}; }
+
+// CHECK-LABEL: define {{[^@]*}}void @pass_v64bfloat16_sparse
+// CHECK-SAME: ([[STRUCT_V64BFLOAT16_SPARSE:%.*]] [[DOTCOERCE:%.*]]) #[[ATTR0:[0-9]+]] {
+void pass_v64bfloat16_sparse(v64bfloat16_sparse) {}
 
 }
