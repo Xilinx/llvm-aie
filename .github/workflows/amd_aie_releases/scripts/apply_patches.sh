ec9f2de158fc9cd0e64df4e7eea4844a8544f3be
makslevental <maksim.levental@gmail.com>
2024-04-30 14:52:50 -0500

    [WIP] wheel distro

---
+++ .github/workflows/amd_aie_releases/scripts/apply_patches.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -uxo pipefail
+
+# note that space before slash is important
+PATCHES="\
+mscv \
+"
+
+if [[ x"${APPLY_PATCHES:-true}" == x"true" ]]; then
+  for PATCH in $PATCHES; do
+    echo "applying $PATCH"
+    git apply --quiet --ignore-space-change --ignore-whitespace --directory llvm-aie patches/$PATCH.patch
+    ERROR=$?
+    if [ $ERROR != 0 ]; then
+      git apply --ignore-space-change --ignore-whitespace --verbose --directory llvm-aie patches/$PATCH.patch -R --check
+      ERROR=$?
+      if [ $ERROR != 0 ]; then
+        exit $ERROR
+      fi
+    fi
+  done
+fi
