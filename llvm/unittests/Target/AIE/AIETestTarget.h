345a3d10efe33fb5e6dbea27354eeae8a525da70
Gaëtan Bossu <gaetan.bossu@amd.com>
2024-07-03 13:41:29 +0100

    [AIEX] Define AIETest target for scheduling unit-tests
    And use it to verify scheduling with DeltaCycles. We had unit tests for
    the MI insertion logic in ScheduleDAGMITestUtils.cpp, but it really just
    verifies movePickedSU(). Here we test a larger chunk of the scheduler.

---
+++ llvm/unittests/Target/AIE/AIETestTarget.h
@@ -0,0 +1,18 @@
+//===- AIETestTarget.h ----------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2024 Advanced Micro Devices, Inc. or its affiliates
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Target/TargetMachine.h"
+
+namespace llvm::AIE {
+
+/// Create an AIE-like target that can be used for unit-tests.
+LLVMTargetMachine *createAIETestTargetMachine();
+
+} // namespace llvm::AIE
