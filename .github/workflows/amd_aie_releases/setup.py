d46ccb6589d2d19fe9ca1a23c695c9b81132fa2d
Maksim Levental <maksim.levental@gmail.com>
2024-07-03 00:17:53 -0500

    Update amd-aie-distro.yml

--- .github/workflows/amd_aie_releases/setup.py
+++ .github/workflows/amd_aie_releases/setup.py
@@ -22,54 +22,6 @@ class CMakeExtension(Extension):
         self.sourcedir = os.fspath(Path(sourcedir).resolve())
 
 
-def get_cross_cmake_args():
-    cmake_args = {}
-
-    def native_tools():
-        nonlocal cmake_args
-
-        native_tools_dir = Path(sys.prefix).absolute() / "bin"
-        assert native_tools_dir is not None, "native_tools_dir missing"
-        assert os.path.exists(native_tools_dir), "native_tools_dir doesn't exist"
-        cmake_args["LLVM_USE_HOST_TOOLS"] = "ON"
-        cmake_args["LLVM_NATIVE_TOOL_DIR"] = str(native_tools_dir)
-
-    CIBW_ARCHS = os.environ.get("CIBW_ARCHS")
-    if CIBW_ARCHS in {"arm64", "aarch64", "ARM64"}:
-        ARCH = cmake_args["LLVM_TARGETS_TO_BUILD"] = "AArch64"
-    elif CIBW_ARCHS in {"x86_64", "AMD64"}:
-        ARCH = cmake_args["LLVM_TARGETS_TO_BUILD"] = "X86"
-    else:
-        raise ValueError(f"unknown CIBW_ARCHS={CIBW_ARCHS}")
-    if CIBW_ARCHS != platform.machine():
-        cmake_args["CMAKE_SYSTEM_NAME"] = platform.system()
-
-    if platform.system() == "Darwin":
-        if ARCH == "AArch64":
-            cmake_args["CMAKE_OSX_ARCHITECTURES"] = "arm64"
-            cmake_args["LLVM_DEFAULT_TARGET_TRIPLE"] = "arm64-apple-darwin21.6.0"
-            cmake_args["LLVM_HOST_TRIPLE"] = "arm64-apple-darwin21.6.0"
-        elif ARCH == "X86":
-            cmake_args["CMAKE_OSX_ARCHITECTURES"] = "x86_64"
-            cmake_args["LLVM_DEFAULT_TARGET_TRIPLE"] = "x86_64-apple-darwin"
-            cmake_args["LLVM_HOST_TRIPLE"] = "x86_64-apple-darwin"
-    elif platform.system() == "Linux":
-        if ARCH == "AArch64":
-            cmake_args["LLVM_DEFAULT_TARGET_TRIPLE"] = "aarch64-linux-gnu"
-            cmake_args["LLVM_HOST_TRIPLE"] = "aarch64-linux-gnu"
-            cmake_args["CMAKE_C_COMPILER"] = "aarch64-linux-gnu-gcc"
-            cmake_args["CMAKE_CXX_COMPILER"] = "aarch64-linux-gnu-g++"
-            cmake_args["CMAKE_CXX_FLAGS"] = "-static-libgcc -static-libstdc++"
-            native_tools()
-        elif ARCH == "X86":
-            cmake_args["LLVM_DEFAULT_TARGET_TRIPLE"] = "x86_64-unknown-linux-gnu"
-            cmake_args["LLVM_HOST_TRIPLE"] = "x86_64-unknown-linux-gnu"
-
-    cmake_args["LLVM_TARGET_ARCH"] = ARCH
-
-    return cmake_args
-
-
 def get_exe_suffix():
     if platform.system() == "Windows":
         suffix = ".exe"
@@ -102,6 +54,7 @@ class CMakeBuild(build_ext):
             "-DLLVM_BUILD_BENCHMARKS=OFF",
             "-DLLVM_BUILD_EXAMPLES=OFF",
             f"-DLLVM_BUILD_TESTS={RUN_TESTS}",
+            "-DLLVM_BUILD_LLVM_DYLIB=ON",
             "-DLLVM_BUILD_TOOLS=ON",
             "-DLLVM_BUILD_UTILS=ON",
             "-DLLVM_CCACHE_BUILD=ON",
@@ -110,11 +63,12 @@ class CMakeBuild(build_ext):
             "-DLLVM_ENABLE_ZSTD=OFF",
             "-DLLVM_INCLUDE_BENCHMARKS=OFF",
             "-DLLVM_INCLUDE_EXAMPLES=OFF",
+            "-DLLVM_LINK_LLVM_DYLIB=ON",
             f"-DLLVM_INCLUDE_TESTS={RUN_TESTS}",
+            "-DLLVM_ENABLE_WARNINGS=ON",
             "-DLLVM_INCLUDE_TOOLS=ON",
             "-DLLVM_INCLUDE_UTILS=ON",
             "-DLLVM_INSTALL_UTILS=ON",
-            "-DLLVM_ENABLE_WARNINGS=ON",
             # get rid of that annoying af git on the end of .17git
             "-DLLVM_VERSION_SUFFIX=",
             # Disables generation of "version soname" (i.e. libFoo.so.<version>), which
@@ -125,6 +79,7 @@ class CMakeBuild(build_ext):
             f"-DPython3_EXECUTABLE={PYTHON_EXECUTABLE}",
             f"-DCMAKE_BUILD_TYPE={cfg}",  # not used on MSVC, but no harm
         ]
+
         if platform.system() == "Windows":
             cmake_args += [
                 "-DCMAKE_C_COMPILER=cl",
@@ -135,9 +90,6 @@ class CMakeBuild(build_ext):
                 "-DLLVM_USE_CRT_MINSIZEREL=MT",
                 "-DLLVM_USE_CRT_RELEASE=MT",
             ]
-
-        cmake_args_dict = get_cross_cmake_args()
-        cmake_args += [f"-D{k}={v}" for k, v in cmake_args_dict.items()]
 
         if "CMAKE_ARGS" in os.environ:
             cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
@@ -202,7 +154,14 @@ class CMakeBuild(build_ext):
             shutil.copytree(build_temp / "bin", install_dir / "bin")
         else:
             subprocess.run(
-                ["cmake", "--build", ".", "--target", "install", *build_args],
+                [
+                    "cmake",
+                    "--build",
+                    ".",
+                    "--target",
+                    "install-distribution",
+                    *build_args,
+                ],
                 cwd=build_temp,
                 check=True,
             )
@@ -273,5 +232,5 @@ setup(
     ext_modules=[CMakeExtension("llvm-aie", sourcedir="llvm-aie/llvm")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
-    download_url=llvm_url
+    download_url=llvm_url,
 )
