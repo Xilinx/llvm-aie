675e03acb1dd18326eaff245fcc219ac11cc816e
Maksim Levental <maksim.levental@gmail.com>
2024-07-08 23:41:41 -0500

    Update Peano-AIE.cmake

--- clang/cmake/caches/Peano-AIE.cmake
+++ clang/cmake/caches/Peano-AIE.cmake
@@ -46,19 +46,21 @@ set(LLVM_TOOLCHAIN_TOOLS
   opt
   CACHE STRING "")
 
-set(LLVM_DISTRIBUTION_COMPONENTS
+set(_llvm_distribution_components
   clang
   lld
   clang-resource-headers
   builtins-aie2-none-unknown-elf
   runtimes-aie2-none-unknown-elf
-  ${LLVM_TOOLCHAIN_TOOLS}
-  CACHE STRING "")
+  ${LLVM_TOOLCHAIN_TOOLS})
 
 option(LLVM_BUILD_LLVM_DYLIB "" OFF)
 if(LLVM_BUILD_LLVM_DYLIB)
-  list(APPEND LLVM_DISTRIBUTION_COMPONENTS LLVM)
-  set(CLANG_LINK_CLANG_DYLIB OFF CACHE BOOL "")
+  list(APPEND _llvm_targets_to_build LLVM clang-cpp)
 endif()
+
+# there's some bug here where if you list(APPEND ...) to a CACHE variable
+# it doesn't work (neither libLLVM nor clang-cpp were being successfully installed)
+set(LLVM_DISTRIBUTION_COMPONENTS ${_llvm_targets_to_build} CACHE STRING "")
 
 message(STATUS "LLVM_DISTRIBUTION_COMPONENTS: ${LLVM_DISTRIBUTION_COMPONENTS}")
