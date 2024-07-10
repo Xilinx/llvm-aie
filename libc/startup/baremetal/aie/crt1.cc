f3482257d374eb2c7ac152f3350aee3a79c84d22
Stephen Neuendorffer <stephen.neuendorffer@amd.com>
2024-07-10 14:38:03 -0700

    [AIE1] Add startup code
    The shared driver code assumes this exists now.

---
+++ libc/startup/baremetal/aie/crt1.cc
@@ -0,0 +1,12 @@
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2024 Advanced Micro Devices, Inc. or its affiliates
+
+extern "C" {
+extern int main(int, char **);
+void _Exit(int) { done(); }
+void _main_init() { _Exit(main(0, nullptr)); }
+}
