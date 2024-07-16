192e7635add46008a6512e40a1074102d842f24f
Martien de Jong <martien.de-jong@xilinx.com>
2024-07-16 16:00:50 +0200

    [AIE] Unrelated tidying
    Copyright notice, namespace markings, variable casing, ...

--- llvm/lib/Target/AIE/AIEHazardRecognizer.h
+++ llvm/lib/Target/AIE/AIEHazardRecognizer.h
@@ -186,6 +186,8 @@ public:
                 const MachineRegisterInfo &MRI, int DeltaCycles) const;
 
 protected:
+  ScheduleHazardRecognizer::HazardType getHazardType(const MCInstrDesc &Desc,
+                                                     int DeltaCycles);
   static bool
   checkConflict(const ResourceScoreboard<FuncUnitWrapper> &Scoreboard,
                 const InstrItineraryData *ItinData, unsigned SchedClass,
