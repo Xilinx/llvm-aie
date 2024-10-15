f9309d2e258e78039b520aa9b823123399250539
Martien de Jong <martien.de-jong@xilinx.com>
2024-10-15 14:56:27 +0200

    [AIE PostPipeliner] compute and use ResMII for a pipeline candidate loop

--- llvm/lib/Target/AIE/AIEInterBlockScheduling.cpp
+++ llvm/lib/Target/AIE/AIEInterBlockScheduling.cpp
@@ -592,9 +592,12 @@ SchedulingStage InterBlockScheduling::updateScheduling(BlockState &BS) {
 
   // The loop schedule has converged, so we could declare our work done.
   // But first try SWP
-  if (BS.getRegions().size() == 1 && BS.getPostSWP().canAccept(*BS.TheBlock)) {
-    BS.FixPoint.II = 1;
-    return BS.FixPoint.Stage = SchedulingStage::Pipelining;
+  if (BS.getRegions().size() == 1) {
+    auto &PostSWP = BS.getPostSWP();
+    if (PostSWP.canAccept(*BS.TheBlock)) {
+      BS.FixPoint.II = PostSWP.getResMII(*BS.TheBlock);
+      return BS.FixPoint.Stage = SchedulingStage::Pipelining;
+    }
   }
   return BS.FixPoint.Stage = SchedulingStage::SchedulingDone;
 }
