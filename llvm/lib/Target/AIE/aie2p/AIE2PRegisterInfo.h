cf3a19ab9d3c501e7b8c133fdd5dec892d097d2e
Fabian Stuckmann <fabian.stuckmann@amd.com>
2025-01-29 08:00:12 +0000

    [AIE2P][simplify CR] added simplification of control register

--- llvm/lib/Target/AIE/aie2p/AIE2PRegisterInfo.h
+++ llvm/lib/Target/AIE/aie2p/AIE2PRegisterInfo.h
@@ -4,7 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// (c) Copyright 2024 Advanced Micro Devices, Inc. or its affiliates
+// (c) Copyright 2024-2025 Advanced Micro Devices, Inc. or its affiliates
 //
 //===----------------------------------------------------------------------===//
 //
@@ -101,6 +101,8 @@ struct AIE2PRegisterInfo : public AIE2PGenRegisterInfo {
   bool isVecOrAccRegClass(const TargetRegisterClass &RC) const override;
 
   bool isFifoPhysReg(const Register Reg) const override;
+
+  bool isSimplifiableReservedReg(MCRegister PhysReg) const override;
 };
 } // namespace llvm
 
