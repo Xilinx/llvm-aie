315d8eb0a9b8d55f8f617d5547aa3f0fcaef73cf
Gaëtan Bossu <gaetan.bossu@amd.com>
2025-02-04 10:54:00 +0000

    [AIE2p] IRTranslator: Generate MMOs for FIFO ld/st

--- llvm/lib/Target/AIE/aie2p/AIE2PISelLowering.h
+++ llvm/lib/Target/AIE/aie2p/AIE2PISelLowering.h
@@ -4,7 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// (c) Copyright 2024 Advanced Micro Devices, Inc. or its affiliates
+// (c) Copyright 2024-2025 Advanced Micro Devices, Inc. or its affiliates
 //
 //===----------------------------------------------------------------------===//
 //
@@ -31,6 +31,10 @@ public:
   // that properly implements CTLZ.
   // I wonder why this isn't the default.
   bool isCheapToSpeculateCtlz(Type *) const override { return true; }
+
+  bool getTgtMemIntrinsic(IntrinsicInfo &Info, const CallInst &I,
+                          MachineFunction &MF,
+                          unsigned Intrinsic) const override;
 };
 
 } // namespace llvm
