67446ba9f5145cb7fa2e0fe87bb6a0d62cc427eb
Martien de Jong <martien.de-jong@xilinx.com>
2025-03-11 10:49:13 +0100

    [AIE] Some more methods on SlotCounts
    Scalar multiplication
    Nonconst indexing
    distance
    
    add SlotCounts::distance

--- llvm/lib/Target/AIE/AIESlotCounts.cpp
+++ llvm/lib/Target/AIE/AIESlotCounts.cpp
@@ -4,7 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// (c) Copyright 2024 Advanced Micro Devices, Inc. or its affiliates
+// (c) Copyright 2024-2025 Advanced Micro Devices, Inc. or its affiliates
 //
 //===----------------------------------------------------------------------===//
 
@@ -28,12 +28,52 @@ SlotCounts::SlotCounts(const SlotCounts &Org) : Size(Org.Size) {
   }
 }
 
-int SlotCounts::max() {
+int &SlotCounts::operator[](int I) {
+  // Grow to the required size, zero-filling new elements
+  while (I >= Size) {
+    Counts[Size++] = 0;
+  }
+  return Counts[I];
+}
+
+const int &SlotCounts::operator[](int I) const {
+  assert(I < Size);
+  return Counts[I];
+}
+
+int SlotCounts::max() const {
   int Max = 0;
   for (int I = 0; I < Size; I++) {
-    Max = std::max(Max, int(Counts[I]));
+    Max = std::max(Max, Counts[I]);
   }
   return Max;
+}
+
+int SlotCounts::maxIndex() const {
+  int MaxIdx = 0;
+  for (int I = 1; I < Size; I++) {
+    if (Counts[I] > Counts[MaxIdx]) {
+      MaxIdx = I;
+    }
+  }
+  return MaxIdx;
+}
+
+int SlotCounts::totals() const {
+  int Totals = 0;
+  for (int I = 0; I < Size; I++) {
+    Totals += Counts[I];
+  }
+  return Totals;
+}
+
+int SlotCounts::distance(const SlotCounts &Other) const {
+  int Sum = 0;
+  const int N = std::max(Size, Other.Size);
+  for (int I = 0; I < N; ++I) {
+    Sum += abs(at(I) - Other.at(I));
+  }
+  return Sum;
 }
 
 SlotCounts &SlotCounts::operator+=(const SlotCounts &Other) {
@@ -51,9 +91,45 @@ SlotCounts &SlotCounts::operator+=(const SlotCounts &Other) {
   return *this;
 }
 
+SlotCounts &SlotCounts::operator-=(const SlotCounts &Other) {
+  // The common part
+  for (int I = 0; I < Size && I < Other.Size; I++) {
+    Counts[I] -= Other.Counts[I];
+  }
+  // Any excess from the other
+  while (Size < Other.Size) {
+    Counts[Size] = -Other.Counts[Size];
+    Size++;
+  }
+  assert(Size >= Other.Size);
+  assert(Size < MaxSlots);
+  return *this;
+}
+
 SlotCounts SlotCounts::operator+(const SlotCounts &Other) const {
   SlotCounts Result(*this);
   return Result += Other;
+}
+
+SlotCounts SlotCounts::operator-(const SlotCounts &Other) const {
+  SlotCounts Result(*this);
+  return Result -= Other;
+}
+
+SlotCounts &SlotCounts::operator*=(int Scalar) {
+  for (int I = 0; I < Size; I++) {
+    Counts[I] *= Scalar;
+  }
+  return *this;
+}
+
+SlotCounts SlotCounts::operator*(int Scalar) const {
+  SlotCounts Result(*this);
+  return Result *= Scalar;
+}
+
+SlotCounts operator*(int Scalar, const SlotCounts &Slots) {
+  return Slots * Scalar;
 }
 
 } // namespace AIE
