e4a0462593aca5b38e555bbd855d00b47f8c476c
Sagar Maheshwari <sagar.maheshwari@amd.com>
2025-06-24 22:44:37 +0530

    [AIE2P] Enable stack size section for start up code.

--- libc/startup/baremetal/aie2/crt1.cc
+++ libc/startup/baremetal/aie2/crt1.cc
@@ -3,13 +3,10 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// (c) Copyright 2024 Advanced Micro Devices, Inc. or its affiliates
+// (c) Copyright 2024-2025 Advanced Micro Devices, Inc. or its affiliates
 
 extern "C" {
 extern int main(int, char **);
-void _Exit(int val) {
-  (void)val;
-  done();
-}
+void _Exit(int) { done(); }
 void _main_init() { _Exit(main(0, nullptr)); }
 }
