6456a89fb3056ec8a61c8be2e57ad74fccf3870c
Martien de Jong <martien.de-jong@xilinx.com>
2025-07-02 10:43:02 +0200

    [AIE][NFC] Header file to collect the maximum number of resources over all archs

---
+++ llvm/lib/Target/AIE/AIEMaxNumResources.h
@@ -0,0 +1,29 @@
+//===--- AIEMaxNumResources.h - Collect max number of scheduler resources
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// (c) Copyright 2025 Advanced Micro Devices, Inc. or its affiliates
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_AIE_AIEMAXNUMRESOURCES_H
+#define LLVM_LIB_TARGET_AIE_AIEMAXNUMRESOURCES_H
+
+#include <algorithm>
+
+#define GET_NUM_RESOURCES
+#include "AIEGenSubtargetInfo.inc"
+
+#define GET_NUM_RESOURCES
+#include "AIE2GenSubtargetInfo.inc"
+
+#define GET_NUM_RESOURCES
+#include "AIE2PGenSubtargetInfo.inc"
+
+constexpr const int TotalNumResources = std::max(
+    AIE2PItineraries::NumResources,
+    std::max(AIE2Itineraries::NumResources, AIEItineraries::NumResources));
+
+#endif // LLVM_LIB_TARGET_AIE_AIEMAXNUMRESOURCES_H
