90a0f3d5899f0a372b5a14f2467cf5a54f258e51
Fabian Stuckmann <fabian.stuckmann@amd.com>
2025-08-07 03:53:49 -0600

    [AIEX][NFC] outlining methods of AIEClusterBaseAddress

--- llvm/lib/Target/AIE/AIEClusterBaseAddress.cpp
+++ llvm/lib/Target/AIE/AIEClusterBaseAddress.cpp
@@ -74,6 +74,10 @@ static cl::opt<bool> EnableChainsForVectorLdSt(
 
 namespace {
 
+LLT getLoadStoreType(const MachineInstr &MI) {
+  return (*MI.memoperands_begin())->getMemoryType();
+}
+
 /// Try and re-order PTR_ADD instructions to maximise the size of constant
 /// PTR_ADD chains.
 bool optimisePostIncrements(ArrayRef<MachineInstr *> PtrAdds,
@@ -133,39 +137,9 @@ public:
   static char ID;
   AIEClusterBaseAddress() : MachineFunctionPass(ID) {}
 
-  bool runOnMachineFunction(MachineFunction &MF) override {
-    MRI = &MF.getRegInfo();
-    TargetPassConfig &TPC = getAnalysis<TargetPassConfig>();
-    // Enable CSE.
-    GISelCSEAnalysisWrapper &Wrapper =
-        getAnalysis<GISelCSEAnalysisWrapperPass>().getCSEWrapper();
-    auto *CSEInfo = &Wrapper.get(TPC.getCSEConfig());
-    std::unique_ptr<MachineIRBuilder> Builder =
-        CSEInfo ? std::make_unique<CSEMIRBuilder>()
-                : std::make_unique<MachineIRBuilder>();
-    Builder->setMF(MF);
-    MachineIRBuilder &MIB = *Builder;
-    // Set Observer
-    GISelObserverWrapper Observer;
-    if (CSEInfo) {
-      Observer.addObserver(CSEInfo);
-      MIB.setChangeObserver(Observer);
-    }
+  bool runOnMachineFunction(MachineFunction &MF) override;
 
-    bool Changed = false;
-    for (MachineBasicBlock &MBB : MF) {
-      while (processBasicBlock(MBB, MIB, Observer))
-        Changed = true;
-    }
-    return Changed;
-  }
-
-  void getAnalysisUsage(AnalysisUsage &AU) const override {
-    AU.addRequired<MachineModuleInfoWrapperPass>();
-    AU.addRequired<GISelCSEAnalysisWrapperPass>();
-    AU.addRequired<TargetPassConfig>();
-    AU.setPreservesAll();
-  }
+  void getAnalysisUsage(AnalysisUsage &AU) const override;
 
   StringRef getPassName() const override { return AIE_CLUSTER_BASE_ADDRESS; }
 
@@ -175,101 +149,24 @@ private:
   const MachineRegisterInfo *MRI = nullptr;
 
   bool processBasicBlock(MachineBasicBlock &MBB, MachineIRBuilder &MIB,
-                         GISelObserverWrapper &Observer) {
-
-    bool Changed = false;
-
-    // Get all G_PTR_ADDs that use the same pointer.
-    RegUseMap RegAndUses = collectPtrUses(MBB);
-
-    // Optimise instruction order
-    for (auto &RegAndUse : reverse(RegAndUses)) {
-      ArrayRef<MachineInstr *> PtrAdds = RegAndUse.second;
-      Changed |= optimisePostIncrements(PtrAdds, *MRI, MIB, Observer);
-    }
-
-    // Create chains, when profitable.
-    for (auto RegAndUse : RegAndUses) {
-
-      SmallVector<MachineInstr *, 8> &Instrs = RegAndUse.second;
-      // Chaining acceptance criteria.
-      if (shouldSkipChaining(RegAndUse.first, Instrs, MBB))
-        continue;
-
-      // Build chain, breaking it (or restarting it) when necessary
-      Changed |= buildChain(Instrs, MBB, MIB, Observer);
-    }
-    return Changed;
-  }
+                         GISelObserverWrapper &Observer);
 
   // Get all candidates, i.e. groups of G_PTR_ADDs in the same
   // basic block that shares the same input pointer.
-  RegUseMap collectPtrUses(MachineBasicBlock &MBB) {
-    RegUseMap RegAndUses;
-    for (MachineInstr &MI : MBB) {
-      if (MI.getOpcode() == TargetOpcode::G_PTR_ADD)
-        RegAndUses[MI.getOperand(1).getReg()].push_back(&MI);
-    }
-    return RegAndUses;
-  }
+  RegUseMap collectPtrUses(MachineBasicBlock &MBB);
 
   // Evaluate if we consider a group of G_PTR_ADDs as a candidate to
   // create a chain.
   bool shouldSkipChaining(Register PtrReg,
                           const SmallVector<MachineInstr *, 8> &Instrs,
-                          MachineBasicBlock &MBB) {
-
-    // No chain possibility at all.
-    if (Instrs.size() <= 1)
-      return true;
-
-    // If the base reg is used in any of the successive MBBs, then we don't
-    // want to chain the corresponding ptr adds, since this would introduce a
-    // COPY and increase reg pressure.
-    return isRegUsedInSuccessiveMBBs(&MBB, PtrReg);
-  }
+                          MachineBasicBlock &MBB);
 
   // Build a chain (or set of chains) of G_PTR_ADDs. We consider as
   // chain a linear sequence of linked G_PTR_ADDs, tied to output and
   // input pointers.
   bool buildChain(SmallVector<MachineInstr *, 8> &Instrs,
                   MachineBasicBlock &MBB, MachineIRBuilder &MIB,
-                  GISelObserverWrapper &Observer) {
-    bool Changed = false;
-    int64_t AccumulatedOffset = 0;
-    for (unsigned I = 0; I < Instrs.size() - 1; I++) {
-      MachineInstr *MI = Instrs[I];
-      MachineInstr *MINext = Instrs[I + 1];
-      auto OffsetMI =
-          getIConstantVRegValWithLookThrough(MI->getOperand(2).getReg(), *MRI);
-      auto OffsetMINext = getIConstantVRegValWithLookThrough(
-          MINext->getOperand(2).getReg(), *MRI);
-
-      // Evaluate if we should restart the chain from the base
-      // pointer. This is necessary when we deal with unknown offsets
-      // (not constants) and desirable when we share pointers between
-      // loads and stores (avoiding dependencies).
-      if (shouldBreakChain(MI, MINext, OffsetMI, OffsetMINext)) {
-        AccumulatedOffset = 0;
-        continue;
-      }
-
-      AccumulatedOffset += OffsetMI->Value.getSExtValue();
-      const int64_t NewNextOffset =
-          OffsetMINext->Value.getSExtValue() - AccumulatedOffset;
-      MIB.setInsertPt(MBB, MINext->getIterator());
-
-      Register NewOffsetReg =
-          MIB.buildConstant(LLT::scalar(20), NewNextOffset).getReg(0);
-
-      Observer.changingInstr(*MINext);
-      MINext->getOperand(1).setReg(MI->getOperand(0).getReg());
-      MINext->getOperand(2).setReg(NewOffsetReg);
-      Observer.changedInstr(*MINext);
-      Changed = true;
-    }
-    return Changed;
-  }
+                  GISelObserverWrapper &Observer);
 
   // Evaluate if we should break the chain construction.
   // Criteria:
@@ -277,74 +174,210 @@ private:
   //  * Pointer shared between load(s) and store(s).
   bool shouldBreakChain(MachineInstr *MIA, MachineInstr *MIB,
                         std::optional<ValueAndVReg> OffsetA,
-                        std::optional<ValueAndVReg> OffsetB) {
-
-    // If one of the offsets is not constant, it is better to break the chain.
-    if (!OffsetA || !OffsetB)
-      return true;
-
-    return hasMixedLoadStoreUse({MIA, MIB});
-  }
+                        std::optional<ValueAndVReg> OffsetB);
 
   // Return true if the instructions are used by both loads and stores.
-  bool hasMixedLoadStoreUse(SmallVector<MachineInstr *, 2> Instrs) {
-    unsigned LoadCount = 0;
-    unsigned StoreCount = 0;
-    for (MachineInstr *MI : Instrs) {
-      const Register PtrReg = MI->getOperand(0).getReg();
-      for (const MachineInstr &UseMI : MRI->use_instructions(PtrReg)) {
-        if (!UseMI.mayLoadOrStore())
-          continue;
-        if (UseMI.mayLoad())
-          LoadCount++;
-        else
-          StoreCount++;
-        const LLT MemType = getLoadStoreType(UseMI);
-        // If desired, we also can break the chain between pairs of
-        // pointers that are used to load/store vectors and/or scalars.
-        if ((!EnableChainsForScalarLdSt && MemType.isScalar()) ||
-            (!EnableChainsForVectorLdSt && MemType.isVector()))
-          return true;
-      }
-    }
-    return (LoadCount > 0 && StoreCount > 0);
-  }
-
-  LLT getLoadStoreType(const MachineInstr &MI) {
-    return (*MI.memoperands_begin())->getMemoryType();
-  }
+  bool hasMixedLoadStoreUse(SmallVector<MachineInstr *, 2> Instrs);
 
   // Get a set of all reachable MBBs from a given MBB.
   // Loops are handled using the ReachableMBBs set, once we encounter any
   // reachable MBB from a particular MBB, we store it in the set and continue if
   // we find it again. Lastly, we remove the current MBB from the set in case it
   // comes up in the successive basic blocks.
-  std::set<MachineBasicBlock *> findReachableMBBs(MachineBasicBlock *MBB) {
-    std::set<MachineBasicBlock *> ReachableMBBs;
-    SmallVector<MachineBasicBlock *, 8> Worklist;
-    Worklist.append(MBB->succ_begin(), MBB->succ_end());
-    while (!Worklist.empty()) {
-      MachineBasicBlock *CurrMBB = Worklist.pop_back_val();
-      if (!ReachableMBBs.insert(CurrMBB).second)
-        continue;
-      Worklist.append(CurrMBB->succ_begin(), CurrMBB->succ_end());
-    }
-    // Remove the starting MBB from the ReachableMBBs set since we don't want to
-    // be too pessimistic as to not consider uses in the current basic block.
-    ReachableMBBs.erase(MBB);
-    return ReachableMBBs;
-  }
+  std::set<MachineBasicBlock *> findReachableMBBs(MachineBasicBlock *MBB);
 
   // Find if a register is used in reachable MBBs.
-  bool isRegUsedInSuccessiveMBBs(MachineBasicBlock *MBB, Register Reg) {
-    std::set<MachineBasicBlock *> ReachableMBBs = findReachableMBBs(MBB);
-    for (MachineInstr &Use : MRI->use_nodbg_instructions(Reg)) {
-      if (ReachableMBBs.count(Use.getParent()))
+  bool isRegUsedInSuccessiveMBBs(MachineBasicBlock *MBB, Register Reg);
+};
+
+void AIEClusterBaseAddress::getAnalysisUsage(AnalysisUsage &AU) const {
+  AU.addRequired<MachineModuleInfoWrapperPass>();
+  AU.addRequired<GISelCSEAnalysisWrapperPass>();
+  AU.addRequired<TargetPassConfig>();
+  AU.setPreservesAll();
+}
+
+bool AIEClusterBaseAddress::runOnMachineFunction(MachineFunction &MF) {
+  MRI = &MF.getRegInfo();
+  TargetPassConfig &TPC = getAnalysis<TargetPassConfig>();
+  // Enable CSE.
+  GISelCSEAnalysisWrapper &Wrapper =
+      getAnalysis<GISelCSEAnalysisWrapperPass>().getCSEWrapper();
+  auto *CSEInfo = &Wrapper.get(TPC.getCSEConfig());
+  std::unique_ptr<MachineIRBuilder> Builder =
+      CSEInfo ? std::make_unique<CSEMIRBuilder>()
+              : std::make_unique<MachineIRBuilder>();
+  Builder->setMF(MF);
+  MachineIRBuilder &MIB = *Builder;
+  // Set Observer
+  GISelObserverWrapper Observer;
+  if (CSEInfo) {
+    Observer.addObserver(CSEInfo);
+    MIB.setChangeObserver(Observer);
+  }
+
+  bool Changed = false;
+  for (MachineBasicBlock &MBB : MF) {
+    while (processBasicBlock(MBB, MIB, Observer))
+      Changed = true;
+  }
+  return Changed;
+}
+
+bool AIEClusterBaseAddress::processBasicBlock(MachineBasicBlock &MBB,
+                                              MachineIRBuilder &MIB,
+                                              GISelObserverWrapper &Observer) {
+
+  bool Changed = false;
+
+  // Get all G_PTR_ADDs that use the same pointer.
+  RegUseMap RegAndUses = collectPtrUses(MBB);
+
+  // Optimise instruction order
+  for (auto &RegAndUse : reverse(RegAndUses)) {
+    ArrayRef<MachineInstr *> PtrAdds = RegAndUse.second;
+    Changed |= optimisePostIncrements(PtrAdds, *MRI, MIB, Observer);
+  }
+
+  // Create chains, when profitable.
+  for (auto RegAndUse : RegAndUses) {
+
+    SmallVector<MachineInstr *, 8> &Instrs = RegAndUse.second;
+    // Chaining acceptance criteria.
+    if (shouldSkipChaining(RegAndUse.first, Instrs, MBB))
+      continue;
+
+    // Build chain, breaking it (or restarting it) when necessary
+    Changed |= buildChain(Instrs, MBB, MIB, Observer);
+  }
+  return Changed;
+}
+
+AIEClusterBaseAddress::RegUseMap
+AIEClusterBaseAddress::collectPtrUses(MachineBasicBlock &MBB) {
+  RegUseMap RegAndUses;
+  for (MachineInstr &MI : MBB) {
+    if (MI.getOpcode() == TargetOpcode::G_PTR_ADD)
+      RegAndUses[MI.getOperand(1).getReg()].push_back(&MI);
+  }
+  return RegAndUses;
+}
+
+bool AIEClusterBaseAddress::shouldSkipChaining(
+    Register PtrReg, const SmallVector<MachineInstr *, 8> &Instrs,
+    MachineBasicBlock &MBB) {
+
+  // No chain possibility at all.
+  if (Instrs.size() <= 1)
+    return true;
+
+  // If the base reg is used in any of the successive MBBs, then we don't
+  // want to chain the corresponding ptr adds, since this would introduce a
+  // COPY and increase reg pressure.
+  return isRegUsedInSuccessiveMBBs(&MBB, PtrReg);
+}
+
+bool AIEClusterBaseAddress::buildChain(SmallVector<MachineInstr *, 8> &Instrs,
+                                       MachineBasicBlock &MBB,
+                                       MachineIRBuilder &MIB,
+                                       GISelObserverWrapper &Observer) {
+  bool Changed = false;
+  int64_t AccumulatedOffset = 0;
+  for (unsigned I = 0; I < Instrs.size() - 1; I++) {
+    MachineInstr *MI = Instrs[I];
+    MachineInstr *MINext = Instrs[I + 1];
+    auto OffsetMI =
+        getIConstantVRegValWithLookThrough(MI->getOperand(2).getReg(), *MRI);
+    auto OffsetMINext = getIConstantVRegValWithLookThrough(
+        MINext->getOperand(2).getReg(), *MRI);
+
+    // Evaluate if we should restart the chain from the base
+    // pointer. This is necessary when we deal with unknown offsets
+    // (not constants) and desirable when we share pointers between
+    // loads and stores (avoiding dependencies).
+    if (shouldBreakChain(MI, MINext, OffsetMI, OffsetMINext)) {
+      AccumulatedOffset = 0;
+      continue;
+    }
+
+    AccumulatedOffset += OffsetMI->Value.getSExtValue();
+    const int64_t NewNextOffset =
+        OffsetMINext->Value.getSExtValue() - AccumulatedOffset;
+    MIB.setInsertPt(MBB, MINext->getIterator());
+
+    Register NewOffsetReg =
+        MIB.buildConstant(LLT::scalar(20), NewNextOffset).getReg(0);
+
+    Observer.changingInstr(*MINext);
+    MINext->getOperand(1).setReg(MI->getOperand(0).getReg());
+    MINext->getOperand(2).setReg(NewOffsetReg);
+    Observer.changedInstr(*MINext);
+    Changed = true;
+  }
+  return Changed;
+}
+
+bool AIEClusterBaseAddress::shouldBreakChain(
+    MachineInstr *MIA, MachineInstr *MIB, std::optional<ValueAndVReg> OffsetA,
+    std::optional<ValueAndVReg> OffsetB) {
+
+  // If one of the offsets is not constant, it is better to break the chain.
+  if (!OffsetA || !OffsetB)
+    return true;
+
+  return hasMixedLoadStoreUse({MIA, MIB});
+}
+
+bool AIEClusterBaseAddress::hasMixedLoadStoreUse(
+    SmallVector<MachineInstr *, 2> Instrs) {
+  unsigned LoadCount = 0;
+  unsigned StoreCount = 0;
+  for (MachineInstr *MI : Instrs) {
+    const Register PtrReg = MI->getOperand(0).getReg();
+    for (const MachineInstr &UseMI : MRI->use_instructions(PtrReg)) {
+      if (!UseMI.mayLoadOrStore())
+        continue;
+      if (UseMI.mayLoad())
+        LoadCount++;
+      else
+        StoreCount++;
+      const LLT MemType = getLoadStoreType(UseMI);
+      // If desired, we also can break the chain between pairs of
+      // pointers that are used to load/store vectors and/or scalars.
+      if ((!EnableChainsForScalarLdSt && MemType.isScalar()) ||
+          (!EnableChainsForVectorLdSt && MemType.isVector()))
         return true;
     }
-    return false;
   }
-};
+  return (LoadCount > 0 && StoreCount > 0);
+}
+
+std::set<MachineBasicBlock *>
+AIEClusterBaseAddress::findReachableMBBs(MachineBasicBlock *MBB) {
+  std::set<MachineBasicBlock *> ReachableMBBs;
+  SmallVector<MachineBasicBlock *, 8> Worklist;
+  Worklist.append(MBB->succ_begin(), MBB->succ_end());
+  while (!Worklist.empty()) {
+    MachineBasicBlock *CurrMBB = Worklist.pop_back_val();
+    if (!ReachableMBBs.insert(CurrMBB).second)
+      continue;
+    Worklist.append(CurrMBB->succ_begin(), CurrMBB->succ_end());
+  }
+  // Remove the starting MBB from the ReachableMBBs set since we don't want to
+  // be too pessimistic as to not consider uses in the current basic block.
+  ReachableMBBs.erase(MBB);
+  return ReachableMBBs;
+}
+
+bool AIEClusterBaseAddress::isRegUsedInSuccessiveMBBs(MachineBasicBlock *MBB,
+                                                      Register Reg) {
+  std::set<MachineBasicBlock *> ReachableMBBs = findReachableMBBs(MBB);
+  for (MachineInstr &Use : MRI->use_nodbg_instructions(Reg)) {
+    if (ReachableMBBs.count(Use.getParent()))
+      return true;
+  }
+  return false;
+}
 } // namespace
 
 char AIEClusterBaseAddress::ID = 0;
