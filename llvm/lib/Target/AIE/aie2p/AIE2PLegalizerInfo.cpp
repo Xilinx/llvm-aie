1887df19b7d74819c9cb29b137cf90367fa65bf2
Andreu Carminati <andreu.carminati@amd.com>
2025-09-30 07:04:30 -0600

    [AIE2P] Remove compilation warning
    Due to some unused variables.

--- llvm/lib/Target/AIE/aie2p/AIE2PLegalizerInfo.cpp
+++ llvm/lib/Target/AIE/aie2p/AIE2PLegalizerInfo.cpp
@@ -141,7 +141,6 @@ static const LLT V16S32 = LLT::fixed_vector(16, 32);
 static const LLT V32S16 = LLT::fixed_vector(32, 16);
 static const LLT V64S8 = LLT::fixed_vector(64, 8);
 static const LLT V2S256 = LLT::fixed_vector(2, 256);
-static const LLT V8S64 = LLT::fixed_vector(8, 64);
 
 // 512-bit accumulators
 static const LLT AccV1632 = LLT::fixed_vector(16, 32);
@@ -152,7 +151,6 @@ static const LLT V32S32 = LLT::fixed_vector(32, 32);
 static const LLT V64S16 = LLT::fixed_vector(64, 16);
 static const LLT V128S8 = LLT::fixed_vector(128, 8);
 static const LLT V2S512 = LLT::fixed_vector(2, 512);
-static const LLT V16S64 = LLT::fixed_vector(16, 64);
 
 // 1024-bit accumulators
 static const LLT AccV32S32 = LLT::fixed_vector(32, 32);
