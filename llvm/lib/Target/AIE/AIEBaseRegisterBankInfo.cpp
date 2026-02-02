4fa7848d7cd05ab877da4de6ce666699c9fd1054
Fabian Stuckmann <fabian.stuckmann@amd.com>
2026-02-02 06:04:42 -0700

    [AIEX] do not use GPRs when encountering 20 bit constants

--- llvm/lib/Target/AIE/AIEBaseRegisterBankInfo.cpp
+++ llvm/lib/Target/AIE/AIEBaseRegisterBankInfo.cpp
@@ -4,7 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// (c) Copyright 2023-2024 Advanced Micro Devices, Inc. or its affiliates
+// (c) Copyright 2023-2026 Advanced Micro Devices, Inc. or its affiliates
 //
 //===----------------------------------------------------------------------===//
 /// \file
@@ -427,11 +427,6 @@ AIEBaseRegisterBankInfo::getInstrMapping(const MachineInstr &MI) const {
   case TargetOpcode::G_FRAME_INDEX: {
     OpRegBankIdx = {PMI_PTR};
     break;
-  }
-  case TargetOpcode::G_CONSTANT: {
-    if (OpSize[0] != 20)
-      break;
-    [[fallthrough]];
   }
   case TargetOpcode::G_PTRTOINT:
   case TargetOpcode::G_TRUNC: {
