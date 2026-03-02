98f753e050deb105529f591e79c5f95959759da3
Abnikant Singh <abnikant.singh@amd.com>
2026-03-02 11:19:36 +0530

    fixup! bitreverse for non-power of 2.

--- llvm/lib/Target/AIE/aie2p/AIE2PLegalizerInfo.cpp
+++ llvm/lib/Target/AIE/aie2p/AIE2PLegalizerInfo.cpp
@@ -291,7 +291,9 @@ AIE2PLegalizerInfo::AIE2PLegalizerInfo(const AIE2PSubtarget &ST)
       .clampScalar(0, S32, S32)
       .clampScalar(1, S32, S32);
 
-  getActionDefinitionsBuilder(G_BITREVERSE).lower();
+  getActionDefinitionsBuilder(G_BITREVERSE)
+      .lowerFor({S16, S32})
+      .clampScalar(0, S32, S32);
   getActionDefinitionsBuilder(G_BSWAP).lower();
 
   getActionDefinitionsBuilder(G_TRUNC)
