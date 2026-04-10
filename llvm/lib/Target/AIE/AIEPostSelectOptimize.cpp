b159e4e4354f9c7b3a8f0d2b50eb1d4269feaa96
Niwin Anto <niwianto@amd.com>
2026-04-10 02:39:34 -0600

    [AIEX]Propagate MMOs for 4-way load with chain of copies

--- llvm/lib/Target/AIE/AIEPostSelectOptimize.cpp
+++ llvm/lib/Target/AIE/AIEPostSelectOptimize.cpp
@@ -4,7 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// (c) Copyright 2024 Advanced Micro Devices, Inc. or its affiliates
+// (c) Copyright 2024-2026 Advanced Micro Devices, Inc. or its affiliates
 //
 //===----------------------------------------------------------------------===//
 /// \file
@@ -602,8 +602,9 @@ bool traverseVecPointerChain(Register Reg, SmallPtrSet<const Value *, 4> &GVSet,
                              const AIEBaseInstrInfo *TII,
                              MachineRegisterInfo &MRI) {
   const MachineInstr *MI = MRI.getVRegDef(Reg);
-  // Skip subvector copy.
-  if (MI->isCopy() && MI->getOperand(1).getReg().isVirtual())
+  // Skip through chains of virtual-register copies (including subregister
+  // extracts) so we reach the defining operation.
+  while (MI->isCopy() && MI->getOperand(1).getReg().isVirtual())
     MI = MRI.getVRegDef(MI->getOperand(1).getReg());
 
   OptionalOp VOp = TII->parseAbstractOp(*MI);
