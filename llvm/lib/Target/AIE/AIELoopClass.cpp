cd64497523c77a6098ee8fb52be4a39812806d25
Fabian Stuckmann <fabian.stuckmann@amd.com>
2026-04-16 02:01:35 -0600

    [AIEX] loop class log update

--- llvm/lib/Target/AIE/AIELoopClass.cpp
+++ llvm/lib/Target/AIE/AIELoopClass.cpp
@@ -4,7 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// (c) Copyright 2025 Advanced Micro Devices, Inc. or its affiliates
+// (c) Copyright 2025-2026 Advanced Micro Devices, Inc. or its affiliates
 //
 //===----------------------------------------------------------------------===//
 
@@ -108,7 +108,7 @@ int classifyLoop(const SlotStatistics &Stats) {
     }
   }
   int LoopClass = Kernels[Best].Index;
-  LLVM_DEBUG(dbgs() << "LoopClass=" << Best << " Score=" << LoopClass);
+  LLVM_DEBUG(dbgs() << "LoopClass=" << Best << " Score=" << LoopClass << "\n");
   return LoopClass;
 }
 
