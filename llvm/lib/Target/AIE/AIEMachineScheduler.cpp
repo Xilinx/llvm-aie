34e682910e529e27a80e8404fe352e950f08b016
Martien de Jong <martien.de-jong@xilinx.com>
2026-09-14 12:29:53 +0200

    [AIE] Integrate PostRegAlloc in PostPipeliner
    This integrates the components added earlier, in particular RegDefUseTracker,
    ScheduleInterpreter and PostRegalloc.
    
    The postpipeliner runs in two modes on each II, Phys mode and Virt mode.
    
    Phys mode is the original postpipeliner.
    In Virtual mode, we virtualize the selected candidates, then build the DAG

--- llvm/lib/Target/AIE/AIEMachineScheduler.cpp
+++ llvm/lib/Target/AIE/AIEMachineScheduler.cpp
@@ -23,6 +23,7 @@
 #include "llvm/CodeGen/MachineInstr.h"
 #include "llvm/CodeGen/MachineScheduler.h"
 #include "llvm/CodeGen/ResourceScoreboard.h"
+#include "llvm/CodeGen/TargetRegisterInfo.h"
 #include "llvm/Support/Debug.h"
 #include <memory>
 
@@ -1776,6 +1777,8 @@ void llvm::AIEPostRASchedStrategy::buildGraph(ScheduleDAGMI &DAG, AAResults *AA,
     // dependences appear as forward dependences between the first and the
     // second iteration.
     NCopies = 2;
+    // Initialize pipelining.
+    BS.initPipelining();
   }
   DEBUG_BLOCKS(dbgs() << "    buildGraph, NCopies=" << NCopies << "\n");
   for (int S = 0; S < NCopies; S++) {
@@ -1856,7 +1859,7 @@ void AIEScheduleDAGMI::schedule() {
 
     auto &PostSWP = BS.getPostSWP();
 
-    if (PostSWP.schedule(*this, BS.FixPoint.II)) {
+    if (PostSWP.schedule(*this, BS.FixPoint.II, BS.FixPoint.PipelinerMode)) {
       BS.setPipelined();
       LLVM_DEBUG(PostSWP.dump());
     }
